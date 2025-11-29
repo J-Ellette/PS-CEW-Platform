c3ffd94f1bb141c31cf2bb8b3e36982193025d14
James Ellette <james.ellette@gmail.com>
2025-11-29 11:05:56 -0600

    Initialize PS-CEW-Platform module with metadata

---
+++ PS-CEW-Platform.psm1
@@ -0,0 +1,13 @@
+@{
+    RootModule = 'PS-CEW-Platform.psm1'
+    ModuleVersion = '0.1.0'
+    GUID = '00000000-0000-0000-0000-000000000000'
+    Author = 'J-Ellette'
+    CompanyName = 'CEW'
+    Copyright = '(c) 2025'
+    Description = 'PS-CEW-Platform - PowerShell 7 command-centric file manager platform'
+    FunctionsToExport = @('*')
+    CompatiblePSEditions = @('Core')
+    PowerShellVersion = '7.0'
+    FileList = @()
+}
